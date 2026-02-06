220e840a10c7d488a8eee669d058bbd160343369
Omkar Shenwai <omkarshenwai322@gmail.com>
2026-02-07 00:30:11 +0530

    Time: 35 ms (48.84%), Space: 104.9 MB (86.63%) - LeetHub

---
+++ 3634-minimum-removals-to-balance-array/3634-minimum-removals-to-balance-array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minRemoval(vector<int>& nums, int k) {
+     int n = nums.size();
+     sort(nums.begin(),nums.end());
+      int i=0;
+      int maxlen = 0;
+      for(int j=0;j<n;j++){
+        while(nums[j]>(long long)nums[i]*k){
+           i++; 
+        }
+        maxlen = max(maxlen,j-i+1);
+      }    
+      return n - maxlen;  
+    }
+};
